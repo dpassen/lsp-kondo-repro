f00c52b964c428db9bc8591e1a0e64eabfa4d85d
Derek Passen <dpassen1@gmail.com>
2022-01-20 16:28:15 -0600

    Remove unneeded project keys

--- project.clj
+++ project.clj
@@ -2,11 +2,7 @@
   :description "LSP kondo repro"
   :dependencies
   [[org.clojure/clojure "1.10.3"]]
-  :pedantic? true
   :profiles
   {:dev
    {:dependencies
-    [[com.gfredericks/test.chuck "0.2.13"]]}
-   :uberjar
-   {:aot :all}}
-  :main lsp-kondo-repro.core)
+    [[com.gfredericks/test.chuck "0.2.13"]]}})
