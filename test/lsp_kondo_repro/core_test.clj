c413803fc1cb7a91b01c99143b34caa7aa29a6d9
Derek Passen <dpassen1@gmail.com>
2022-01-20 16:08:25 -0600

    Extreme simplification

--- test/lsp_kondo_repro/core_test.clj
+++ test/lsp_kondo_repro/core_test.clj
@@ -1,16 +1,10 @@
 (ns lsp-kondo-repro.core-test
   (:require
    [clojure.test :refer [deftest]]
-   [clojure.test.check.generators :as gen]
-   [com.gfredericks.test.chuck.clojure-test :refer [checking]]
-   [lsp-kondo-repro.core :as lkr]
-   [testit.core :refer [fact]]))
+   [com.gfredericks.test.chuck.clojure-test :refer [checking]]))
 
 (deftest can-process-charge-commands
-  (checking "illegal charges" 100
-   [amount (gen/fmap bigint (gen/large-integer* {:min (inc limit)}))]
-   (fact "silly test"
-     (+ 1 1) => 2)))
+  (checking "illegal charges" 100))
 
 
 
